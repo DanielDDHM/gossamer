35547c4204547f4c530166b2429893431523fe2c
DanielDDHM <daniel.ddhm@gmail.com>
2025-01-10 16:50:49 +0000

    feat(statement-distribution):Implement FanIn approach to statement distribution

--- dot/parachain/statement-distribution/statement_distribution_test.go
+++ dot/parachain/statement-distribution/statement_distribution_test.go
@@ -1,18 +1,11 @@
 package statementdistribution
 
-// import (
-// 	parachainutil "github.com/ChainSafe/gossamer/dot/parachain/util"
-// )
+// //nolint
+func createStatementDistribution() (*StatementDistribution, chan any) {
 
-// func createStatementDistribution() (*StatementDistribution, chan any) {
-// 	mockAggregator := parachainutil.NewReputationAggregator(func(rep parachainutil.UnifiedReputationChange) bool {
-// 		return rep.Type == parachainutil.Malicious
-// 	})
+	subSystemToOverseer := make(chan any, 10)
 
-// 	subSystemToOverseer := make(chan any, 10)
-
-// 	return &StatementDistribution{
-// 		reputationAggregator: mockAggregator,
-// 		SubSystemToOverseer:  subSystemToOverseer,
-// 	}, subSystemToOverseer
-// }
+	return &StatementDistribution{
+		SubSystemToOverseer: subSystemToOverseer,
+	}, subSystemToOverseer
+}
