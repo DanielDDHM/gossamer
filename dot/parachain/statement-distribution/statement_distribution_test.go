22477cdb8f27f6bde7d4bbcbfd29e0115fdd9fd3
DanielDDHM <daniel.ddhm@gmail.com>
2025-01-10 15:19:28 +0000

    feat(statement-distribution):Implement FanIn approach to statement distribution

--- dot/parachain/statement-distribution/statement_distribution_test.go
+++ dot/parachain/statement-distribution/statement_distribution_test.go
@@ -1,22 +1,22 @@
 package statementdistribution
 
-import (
-	parachainutil "github.com/ChainSafe/gossamer/dot/parachain/util"
-)
+// import (
+// 	parachainutil "github.com/ChainSafe/gossamer/dot/parachain/util"
+// )
 
-func createStatementDistribution() (*StatementDistribution, chan parachainutil.NetworkBridgeTxMessage) {
-	mockAggregator := parachainutil.NewReputationAggregator(func(rep parachainutil.UnifiedReputationChange) bool {
-		return rep.Type == parachainutil.Malicious
-	})
+// func createStatementDistribution() (*StatementDistribution, chan parachainutil.NetworkBridgeTxMessage) {
+// 	mockAggregator := parachainutil.NewReputationAggregator(func(rep parachainutil.UnifiedReputationChange) bool {
+// 		return rep.Type == parachainutil.Malicious
+// 	})
 
-	subSystemToOverseer := make(chan parachainutil.NetworkBridgeTxMessage, 10)
+// 	subSystemToOverseer := make(chan parachainutil.NetworkBridgeTxMessage, 10)
 
-	return &StatementDistribution{
-		reputationAggregator: mockAggregator,
-		SubSystemToOverseer:  subSystemToOverseer,
-	}, subSystemToOverseer
-}
+// 	return &StatementDistribution{
+// 		reputationAggregator: mockAggregator,
+// 		SubSystemToOverseer:  subSystemToOverseer,
+// 	}, subSystemToOverseer
+// }
 
-func createChannels() (chan any, chan any, chan any, chan any) {
-	return make(chan any, 10), make(chan any, 10), make(chan any, 10), make(chan any, 10)
-}
+// func createChannels() (chan any, chan any, chan any, chan any) {
+// 	return make(chan any, 10), make(chan any, 10), make(chan any, 10), make(chan any, 10)
+// }
